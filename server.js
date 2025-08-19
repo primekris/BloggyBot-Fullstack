41c3827356c34bd2ea644d0ec2f3586824615582
Krishna Parihar <studykrishnawork@gmail.com>
2025-08-20 00:30:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post('/api/chat', async (req, res) => {
 
   try {
     const response = await axios.post(
-      "https://api.together.xyz/v1/chat/completions",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_KEY}",
       {
         model,
         messages: [
